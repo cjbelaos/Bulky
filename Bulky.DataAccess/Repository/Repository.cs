b56303523b34eac5419ee698f98a2c68b58bd005
Chris John <chrisjohnbelaos@gmail.com>
2023-07-01 16:36:10 +0800

    Add Shopping Cart and Order Confirmation

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -27,16 +27,23 @@ namespace BulkyBook.DataAccess.Repository
 			dbSet.Add(entity);
 		}
 
-		public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+		public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
 		{
-			IQueryable<T> query = dbSet;
+			IQueryable<T> query;
+			if (tracked)
+			{
+				query = dbSet;
+			}
+			else
+			{
+				query = dbSet.AsNoTracking();
+			}
 			query = query.Where(filter);
 			if (!string.IsNullOrEmpty(includeProperties))
 			{
 				foreach (var includeProp in includeProperties
 					.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
 				{
-
 					query = query.Include(includeProp);
 				}
 			}
@@ -44,13 +51,17 @@ namespace BulkyBook.DataAccess.Repository
 		}
 
 		//Category, CoverType
-		public IEnumerable<T> GetAll(string? includeProperties = null)
+		public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
 		{
 			IQueryable<T> query = dbSet;
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
 			if (!string.IsNullOrEmpty(includeProperties))
 			{
-				foreach(var includeProp in includeProperties
-					.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+				foreach (var includeProp in includeProperties
+					.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
 				{
 
 					query = query.Include(includeProp);
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -1,10 +1,5 @@
 ﻿using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.Repository.IRepository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
@@ -14,12 +9,20 @@ namespace BulkyBook.DataAccess.Repository
         public ICategoryRepository Category { get; set; }
         public ICompanyRepository Company { get; set; }
         public IProductRepository Product { get; set; }
+        public IShoppingCartRepository ShoppingCart { get; set; }
+        public IApplicationUserRepository ApplicationUser { get; set; }
+        public IOrderHeaderRepository OrderHeader { get; set; }
+        public IOrderDetailRepository OrderDetail { get; set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             Company = new CompanyRepository(_db);
             Product = new ProductRepository(_db);
+			ShoppingCart = new ShoppingCartRepository(_db);
+			ApplicationUser = new ApplicationUserRepository(_db);
+			OrderHeader = new OrderHeaderRepository(_db);
+			OrderDetail = new OrderDetailRepository(_db);
         }
         
         public void Save()
